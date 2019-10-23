2f7ddf82c89288f286a3ca51a0f1c22ee1f8e55c
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-10-23 12:06:05 -0600

    changed date default to

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,30 @@
 // Update with your config settings.
 
 module.exports = {
-
+  
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/corporate-event-planner.sqlite3',
+    client: "sqlite3", connection: {
+      filename: "./data/corporate-event-planner.sqlite3",
+    }, useNullAsDefault: true, migrations: {
+      directory: "./data/migrations",
+    }, seeds: {
+      directory: "./data/seeds",
     },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/test.db3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
+  }, testing: {
+    client: "sqlite3", connection: {
+      filename: "./data/test.db3",
+    }, useNullAsDefault: true, migrations: {
+      directory: "./data/migrations",
+    }, seeds: {
+      directory: "./data/seeds",
     },
   },
-
+  
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './database/seeds',
-    },
-    useNullAsDefault: true,
+    client: "pg", connection: process.env.DATABASE_URL, migrations: {
+      directory: "./data/migrations",
+    }, seeds: {
+      directory: "./data/seeds",
+    }, useNullAsDefault: true,
   },
 };
